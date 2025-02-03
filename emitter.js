16c2469e42af82a5063c14447b2f1d6cdc8727af
Alex-Doro <alexandr.doroshenko13@gmail.com>
2025-02-03 15:16:40 +0200

    hw4 - write in log with async emitter

--- emitter.js
+++ emitter.js
@@ -1,55 +1,52 @@
+export default class EventEmitter {
+  constructor() {
+    this.events = new Map();
+  }
 
-class EventEmitter {
-
-    constructor() {
-        this.events = new Map()
+  on(eventName, listener) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
     }
 
-    on(eventName, listener) {
-        if (!this.events[eventName]) {
-            this.events[eventName] = []
-        }
+    this.events[eventName].push(listener);
+  }
 
-        this.events[eventName].push(listener)
-    }
+  emit(eventName, ...arg) {
+    if (!this.events[eventName]) return false;
 
-    emit(eventName, ...arg) {
-        if (!this.events[eventName]) return false
+    this.events[eventName].forEach(listener => {
+      listener(...arg);
+    });
+  }
 
-        this.events[eventName].forEach(listener => {
-            listener(...arg)
-        })
-    }
+  off(eventName, listener) {
+    if (!this.events[eventName]) return false;
 
-    off(eventName, listener) {
-        if (!this.events[eventName]) return false
-
-        this.events[eventName] = this.events[eventName].filter( itm => itm !== listener) 
-    }
+    this.events[eventName] = this.events[eventName].filter(
+      itm => itm !== listener
+    );
+  }
 }
 
-const emitter = new EventEmitter()
+// const emitter = new EventEmitter();
 
-const onHello = async (name) => {
-    
-    console.log(`hello ${name}`)
-}
+// const onHello = async name => {
+//   console.log(`hello ${name}`);
+// };
 
+// const onQuestion = name => {
+//   console.log(`how are you, ${name}?`);
+// };
 
-const onQuestion = (name) => {
-    console.log(`how are you, ${name}?`)
-}
+// const HELLO_EVENT = "event1";
 
-const HELLO_EVENT = 'event1'
+// emitter.on(HELLO_EVENT, onHello);
+// emitter.on(HELLO_EVENT, onQuestion);
 
-emitter.on(HELLO_EVENT, onHello)
-emitter.on(HELLO_EVENT, onQuestion)
+// emitter.emit(HELLO_EVENT, "Anna");
 
-emitter.emit(HELLO_EVENT, 'Anna')
+// emitter.off(HELLO_EVENT, onHello);
 
-emitter.off(HELLO_EVENT, onHello)
-
-emitter.emit(HELLO_EVENT, 'John')
-
+// emitter.emit(HELLO_EVENT, "John");
 
 // emit("a", 1, true, 'ff')
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import Logger from "./logger/logger.js";
+import levels from "./logger/levels.js";
+import EventEmitter from "./emitter.js";
+
 const logger = new Logger();
+const emitter = new EventEmitter();
 
 process.on("uncaughtException", err => {
   console.log("uncaughted Exception", err.message);
@@ -12,6 +16,28 @@ process.on("unhandledRejection", err => {
 
   process.exit(1);
 });
+
+emitter.on(levels.INFO, msg => {
+  process.nextTick(() => {
+    logger.info(msg);
+  });
+});
+
+emitter.on(levels.WARNING, msg => {
+  process.nextTick(() => {
+    logger.info(msg);
+  });
+});
+
+emitter.on(levels.ERROR, msg => {
+  process.nextTick(() => {
+    logger.info(msg);
+  });
+});
+
+emitter.emit(levels.INFO, "Info message");
+emitter.emit(levels.WARNING, "Warning message");
+emitter.emit(levels.ERROR, "Error message");
 
 // const buffer = Buffer.from("Hello World")
 // const base64 = buffer.toString('base64')
